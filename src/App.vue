ef1bf38e59811807386d3dba75941a40d5e9a87c
tonraki <Keattipong.ng57@snru.ac.th>
2024-03-10 16:15:20 +0700

    update march 2024

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
+  <div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/></div>
 </template>
 
 <script>
